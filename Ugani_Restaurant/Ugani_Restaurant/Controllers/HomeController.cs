3018541d17f382b83d5436d07f6f97fe9880d47c
DiemQuynh <haquynhla2002@gmail.com>
2023-09-26 22:59:35 +0700

    Update Booking

--- Ugani_Restaurant/Ugani_Restaurant/Controllers/HomeController.cs
+++ Ugani_Restaurant/Ugani_Restaurant/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ugani_Restaurant.Models;
 
 namespace Ugani_Restaurant.Controllers
 {
     public class HomeController : Controller
     {
+        private UGANI_1Entities db = new UGANI_1Entities();
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Booking()
+        {
+            return View(db.LOAIKHONGGIANs.ToList());
+        }
+
+        public ActionResult GetTableById(int id)
+        {
+            List<BANAN> bANANs = db.BANANs.Where(m => m.MAKHONGGIAN == id).ToList();
+            return PartialView("_listTable", bANANs);
         }
 
         public ActionResult About()
